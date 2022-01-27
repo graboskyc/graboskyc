cd030333f3bd4946ac819535483c0fb9970758ab
Chris Grabosky <chris@grabosky.net>
2022-01-26 21:39:04 -0500

    blog functionality subject to change on formatting

---
+++ GSKYBlog/DataModels/Blog.cs
@@ -0,0 +1,11 @@
+namespace GSKYBlog.DataModels {
+    public class Blog {
+        public string _id {get;set;}
+        public string type {get;set;}
+        public string status {get;set;}
+        public string plaintext {get;set;}
+        public string feature_image {get;set;}
+        public string title {get;set;}
+        public string slug {get;set;}
+    }
+}
