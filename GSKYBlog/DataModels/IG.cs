0e3fd9d2630622d55cd1820513c6961edfc187a5
Chris Grabosky <chris@grabosky.net>
2022-01-26 21:10:54 -0500

    basic outline of something working

---
+++ GSKYBlog/DataModels/IG.cs
@@ -0,0 +1,8 @@
+namespace GSKYBlog.DataModels {
+    public class IG {
+        public string _id {get;set;}
+        public string taken_at {get;set;}
+        public string path {get;set;}
+        public string caption {get;set;}
+    }
+}
---
+++ GSKYBlog/wwwroot/js/realm_shim.js
@@ -0,0 +1,33 @@
+const client = stitch.Stitch.initializeDefaultAppClient("ig-epove");
+
+window.login = async function(){
+    try {
+        if (!client.auth.isLoggedIn) {
+            const credential = new stitch.AnonymousCredential();
+            var authedUser = await client.auth.loginWithCredential(credential);
+            if(authedUser) {
+                console.log(`successfully logged in with id: ${authedUser.id}`);
+                return true;
+            }
+            else {
+                console.log(`login failed`);
+                return false;
+            }
+        } else {
+            console.log(client.auth.currentUser);
+            return true;
+        }
+        
+    }
+    catch(e) {
+        return false;
+    }
+}
+
+window.realmShim_Function = async function(fnname, args) {
+    console.log(fnname);
+    console.log(args);
+    var result = await client.callFunction(fnname,args);
+    console.log(result);
+    return result;
+}
