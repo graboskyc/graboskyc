8a70cddd679c8e75a02333e1e2568d491021bc1b
Chris Grabosky <chris@grabosky.net>
2022-02-14 15:09:41 -0500

    fix doc structure

--- BlogPosts/importer.py
+++ BlogPosts/importer.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import json
 import re
 import datetime
+from bson.objectid import ObjectId
 
 load_dotenv()
 URI = os.getenv('CONNSTR')
@@ -21,13 +22,16 @@ newDoc = json.loads(sections[0])
 newDoc["markdown"] = sections[1]
 newDoc["html"] = sections[1]
 newDoc["slug"] = re.sub('[^0-9a-zA-Z]+', '-', newDoc["title"].lower())
-newDoc["published_at"] = datetime.datetime.strptime(newDoc["published_at"], "%Y-%m-%d")
+newDoc["published_at_dt"] = datetime.datetime.strptime(newDoc["published_at"], "%Y-%m-%d")
 
-print(newDoc)
+#print(newDoc)
+print(newDoc["title"])
 
 if("_id" in newDoc):
+    newDoc["id"] = newDoc["_id"]
+    newDoc["_id"] = ObjectId(newDoc["_id"])
     result = handle.replace_one({"_id": newDoc["_id"]}, newDoc)
-    print("Replacing " + newDoc["_id"])
+    print("Replacing " + str(newDoc["_id"]))
 else:
     result = handle.insert_one(newDoc)
     print("Inserted " + str(result.inserted_id))
