d7c2b7fb76f7c408f2f578248a119afea8188db7
Chris Grabosky <chris@grabosky.net>
2024-09-13 11:03:46 -0400

    pics gallery works

--- GSKYBlogServer/DataModels/IG.cs
+++ GSKYBlogServer/DataModels/IG.cs
@@ -1,7 +1,11 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace GSKYBlogServer.DataModels {
+    [BsonIgnoreExtraElements]
     public class IG {
-        public string _id {get;set;}
-        public string taken_at {get;set;}
+        public ObjectId _id {get;set;}
+        public DateTime taken_at {get;set;}
         public string path {get;set;}
         public string caption {get;set;} = null;
         public string location {get;set;} = null;
